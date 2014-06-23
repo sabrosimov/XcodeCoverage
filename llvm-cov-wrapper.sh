1e46896bd71be96b99e4d4347666f89afa67d7e7
Sergey Abrosimov <sabrosimov@five9.com>
2014-06-23 14:05:53 +0400

    Workaround for the crashed GCOV tool

--- llvm-cov-wrapper.sh
+++ llvm-cov-wrapper.sh
@@ -7,9 +7,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+
 if [ "$1" = "-v" ]; then
     echo "llvm-cov-wrapper 4.2.1"
     exit 0
+elif [ "`echo $* | egrep -w 'AppDelegate|SupervisorAPIConnector'`" ] ; then
+    ${DIR}/cla-wrapper.py /usr/bin/gcov-4.2 $*
 else
     ${DIR}/cla-wrapper.py /usr/bin/gcov $*
 fi
