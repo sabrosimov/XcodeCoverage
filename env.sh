97b3ee04064a638f803322db45e6033a21c3b1c8
Sergey Abrosimov <sabrosimov@five9.com>
2014-07-11 16:53:57 +0400

    Adjust after moving the code to Perforce

--- env.sh
+++ env.sh
@@ -1,7 +1,9 @@
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
-export BUILT_PRODUCTS_DIR=$SOURCE_DIR"/SupervisorDashboard-iOS/Supervisor Dashboard/build/Release-iphonesimulator"
 export CURRENT_ARCH="i386"
-export OBJECT_FILE_DIR_normal=$SOURCE_DIR"/SupervisorDashboard-iOS/Supervisor Dashboard/build/Supervisor Dashboard.build/Release-iphonesimulator/Supervisor-cal.build/Objects-normal"
-export OBJROOT=$SOURCE_DIR"/SupervisorDashboard-iOS/Supervisor Dashboard/build"
-export SRCROOT=$SOURCE_DIR"/SupervisorDashboard-iOS/Supervisor Dashboard"
+
+export PRGROOT=$SOURCE_DIR"/supervisor-dashboard"
+export SRCROOT=$PRGROOT"/Supervisor Dashboard"
+export OBJROOT=$SRCROOT"/build"
+export BUILT_PRODUCTS_DIR=$OBJROOT"/Release-iphonesimulator"
+export OBJECT_FILE_DIR_normal=$OBJROOT"/Supervisor Dashboard.build/Release-iphonesimulator/Supervisor-cal.build/Objects-normal"
